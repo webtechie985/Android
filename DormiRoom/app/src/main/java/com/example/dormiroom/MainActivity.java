1537e994efa3ebeabe32fdc594ee7d393f9a846a
webtechie985 <81607744+webtechie985@users.noreply.github.com>
2021-05-07 21:10:31 +0530

    Add files via upload

---
+++ DormiRoom/app/src/main/java/com/example/dormiroom/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.dormiroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.google.com");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
